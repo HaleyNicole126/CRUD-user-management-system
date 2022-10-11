fbe8c9b584122e629dd6ffa0b66cbd4c97e3af88
haybales <haleynicole126@gmail.com>
2022-10-10 22:02:09 -0600

    beginning file structure

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// parsing middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application 
+app.use(bodyParser.json());
+
+// static files
+app.use(express.static('public'));
+
+// Templating Engine
+const handlebars = exphbs.create({ extname: '.hbs', });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+// Router
+app.get('', (req, res) => {
+    res.render('home')
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
